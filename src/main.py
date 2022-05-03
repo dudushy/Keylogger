bb8975e82397f706f68d70d427b39a882583b0eb
Eduardo Talarico <eduardotalarico1337@gmail.com>
2022-05-03 14:33:07 -0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+# pip freeze > requirements.txt
+#* -- Imports
+import os
+
+#* -- Variables
+path = os.path.dirname(__file__) #? Directory path
+
+#* -- Functions
+def clearConsole() -> None: #? Clear console
+    os.system("cls" if os.name == "nt" else "clear")
+
+def main() -> None:
+    pass
+
+#! Main
+if __name__ == "__main__":
+    main()
